e91e19a45e16531627461e66459e0f8584b974be
ivan <ivanlaraorta@gmail.com>
2026-03-10 18:10:10 +0100

    prova vercel

--- src/components/Perfil/Perfil.js
+++ src/components/Perfil/Perfil.js
@@ -5,8 +5,8 @@ import { User } from 'lucide-react';
 const Perfil = () => (
     <div className="card profile-card text-center animate-fade-in">
         <div className="profile-avatar"><User size={40} /></div>
-        <h2 className="profile-name">Torres FC</h2>
-        <p className="profile-status">Pressupost: 454,5 M</p>
+        <h2 className="profile-name">Ivan FC</h2>
+        <p className="profile-status">€454,5 M</p>
     </div>
 );
 
